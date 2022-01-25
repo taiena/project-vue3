631be1766ed58626cae7167573328aa586516fb2
Anna Domaskina <taienametztli@mail.ru>
2022-01-25 19:49:15 +0300

    extract post-item

---
+++ src/components/PostItem.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="Post">
+        <div><strong>Title: </strong>{{ post.title }}</div>
+        <div><strong>Description: </strong>{{ post.body }}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        post: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+.Post {
+    padding: 1rem;
+    margin-bottom: 1rem;;
+    border: 1px solid DarkOrchid;
+    background-color: Ivory;
+}
+</style>
--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,12 +1,22 @@
 <template>
-    <div class="Post" v-for="post in posts" :key="post.id">
-        <div><strong>Title: </strong>{{ post.title }}</div>
-        <div><strong>Description: </strong>{{ post.body }}</div>
+    <div class="PostsContainer">
+        <h3>Posts</h3>
+        <post-item
+            v-for="post in posts"
+            :key="post.id"
+            :post="post"
+        />
     </div>
 </template>
 
 <script>
+import PostItem from "./PostItem"
+
 export default {
+    components: {
+        PostItem
+    },
+
     props: {
         posts: {
             type: Array,
@@ -17,10 +27,14 @@ export default {
 </script>
 
 <style scoped>
-.Post {
-    padding: 1rem;
-    margin-bottom: 1rem;;
-    border: 1px solid DarkOrchid;
-    background-color: Ivory;
+.PostsContainer {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+}
+
+h3 {
+    color: DarkOrchid;
+    margin-bottom: 1rem;
 }
 </style>
